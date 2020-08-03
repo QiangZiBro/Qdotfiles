cd7fb01041852071900781bb7bf86494337fa9d1
QiangZiBro <qiangzibro@gmail.com>
2020-08-03 21:52:59 +0800

    update

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -19,37 +19,29 @@ export LESS_TERMCAP_so=$'\E[1m\E[33m\E[44m'
 # install zplug, plugin manager for zsh, https://github.com/zplug/zplug
 # curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 # zplug configruation
-if [[ ! -d ~/.zplug ]]; then
-  git clone https://github.com/zplug/zplug ~/.zplug
-  # If we can't get zplug, it'll be a very sobering shell experience. To at
-  # least complete the sourcing of this file, we'll define an always-false
-  # returning zplug function.
-  if [[ $? != 0 ]]; then
-    function zplug() {
-      return 1
-    }
-  fi
-fi
-export ZPLUG_HOME=~/.zplug
-if [[ -d "${ZPLUG_HOME}" ]]; then
-  source "${ZPLUG_HOME}/init.zsh"
-fi
-zplug 'plugins/git', from:oh-my-zsh, if:'which git'
-zplug 'romkatv/powerlevel10k', use:powerlevel10k.zsh-theme
-# zplug "plugins/vi-mode", from:oh-my-zsh
-zplug 'zsh-users/zsh-autosuggestions'
-zplug 'zsh-users/zsh-completions', defer:2
-zplug 'zsh-users/zsh-history-substring-search'
-zplug 'zsh-users/zsh-syntax-highlighting', defer:2
-
-if ! zplug check; then
-  zplug install
+if [[ -d ~/.zplug ]]; then
+    export ZPLUG_HOME=~/.zplug
+    if [[ -d "${ZPLUG_HOME}" ]]; then
+      source "${ZPLUG_HOME}/init.zsh"
+    fi
+    zplug 'plugins/git', from:oh-my-zsh, if:'which git'
+    zplug 'romkatv/powerlevel10k', use:powerlevel10k.zsh-theme
+    # zplug "plugins/vi-mode", from:oh-my-zsh
+    zplug 'zsh-users/zsh-autosuggestions'
+    zplug 'zsh-users/zsh-completions', defer:2
+    zplug 'zsh-users/zsh-history-substring-search'
+    zplug 'zsh-users/zsh-syntax-highlighting', defer:2
+    
+    if ! zplug check; then
+      zplug install
+    fi
+    
+    zplug load
+    
+    # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+    [[ ! -f ~/.Qdotfiles/zsh/.p10k.zsh ]] || source ~/.Qdotfiles/zsh/.p10k.zsh
 fi
 
-zplug load
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.Qdotfiles/zsh/.p10k.zsh ]] || source ~/.Qdotfiles/zsh/.p10k.zsh
 #POWERLEVEL9K_MODE='nerdfont-complete'
 #POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 #POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=''
