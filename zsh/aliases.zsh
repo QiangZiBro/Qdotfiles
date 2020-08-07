134b3e2649b8c9e5c05c715ac996da77e8f620a4
QiangZiBro <qiangzibro@gmail.com>
2020-08-07 21:11:37 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,7 @@ common_settings () {
     alias sb="source ~/.zshrc"
     alias eb="nvim ~/.zshrc"
     alias cb="cd ~/.Qdotfiles/zsh/"
+    alias cq="cd ~/.Qdotfiles/"
     alias unproxy='http_proxy="" https_proxy=""'
     alias vinit='nvim ~/.Qdotfiles/scripts/init.sh'
 }
