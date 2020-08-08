f60b38b71a1e9b8868a0295fad373ca81239381b
QiangZiBro <qiangzibro@gmail.com>
2020-08-08 21:36:48 +0800

    update

---
+++ common/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if test "$(uname)" = "Darwin"
+then
+     # mac installation branch
+    echo mac install
+
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+     # linux installation branch
+    echo linux install
+    sudo apt install -y cowsay fortune
+
+fi
