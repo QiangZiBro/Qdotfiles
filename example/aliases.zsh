eccc9f2e81c52556f72c8101c76b29ffa7d4dd6e
QiangZiBro <qiangzibro@gmail.com>
2020-08-08 20:51:01 +0800

    update

--- example/aliases.zsh
+++ example/aliases.zsh
@@ -1,15 +1,9 @@
 common () {
-    alias sb="source ~/.zshrc"
-    alias eb="nvim ~/.zshrc"
-    alias fm="ranger"
+}
+linux () {
 }
 
 mac () {
-    alias nvim="~/applications/nvim-osx64/bin/nvim"
-    alias meshlab="/Applications/meshlab.app/Contents/MacOS/meshlab"
-    alias meshlab="/Applications/meshlab.app/Contents/MacOS/meshlabserver"
-    alias grealpath=realpath
-    alias gsed=sed
 }
 
 common
@@ -20,5 +14,6 @@ then
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
     # linux branch
+    linux
 
 fi
--- example/install.sh
+++ example/install.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
-
-# Smartly use code below, this code can't find command for alias
-if ! command -v  conda &> /dev/null
+if test "$(uname)" = "Darwin"
 then
-    # installation process...
-    if test "$(uname)" = "darwin"
-    then
-         # mac installation branch
-        echo mac install
+     # mac installation branch
+    echo mac install
 
-    elif test "$(expr substr $(uname -s) 1 5)" = "linux"
-    then
-         # linux installation branch
-        echo linux install
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+     # linux installation branch
+    echo linux install
 
-    fi
 fi
-
-# BUGGY!!!!! 
-#    though you can find alias output results in command line, but when you run this file, nothing
-#    can be done
-# Don't use now!
-# check_exists() {
-#     local COMMAND=$1
-#     if ! command -v  $COMMAND &> /dev/null
-#     then
-#         alias $COMMAND >/dev/null 2>&1 && echo "$1 is set as an alias" || echo "$1 is not an alias"
-#         if ! alias $COMMAND >/dev/null 2>&1
-#         then
-#             echo "$COMMAND is not an alias"
-#         fi
-#     fi
-# }
-
-
-# check_exists _zlua
