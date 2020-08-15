bf9399d38f15c0087bc98f882bc72560e579142c
QiangZiBro <qiangzibro@gmail.com>
2020-08-15 17:09:49 +0800

    update from ci

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,10 +2,8 @@
 
 cd "$(dirname $0)"/..
 
-#cp ~/.config/nvim/init.vim neovim
-
+cp ~/.config/nvim/init.vim neovim
 cp ~/.zshrc zsh
-cp ~/.SpaceVim.d/init.toml spacevim
 cp ~/.tmux.conf tmux
 
 if [ -f /etc/privoxy/config ];
