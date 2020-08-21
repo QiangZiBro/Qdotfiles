867f46cd28e0ec41d3357255d76681178ce6a5a5
QiangZiBro <qiangzibro@gmail.com>
2020-08-21 09:59:38 +0800

    update from ci

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -3,17 +3,18 @@ alias dcd="docker-compose up -d"
 alias dce="docker-compose exec"
 alias dcb="docker-compose build"
 
-docker_rm_none(){
-    EXITED_CONTAINER=$(docker ps -a | grep "Exited" | awk '{print $1 }')
-    if [ ! -z "$EXITED_CONTAINER" ];
-    then
-        docker stop  $EXITED_CONTAINER
-        docker rm  $EXITED_CONTAINER 
-    fi
-
-    NONE_IMAGES=$(docker images | grep "none" | awk '{print $3}')
-    if [ ! -z "$NONE_IMAGES" ];
-    then
-        docker rmi  $NONE_IMAGES
-    fi
-}
+#docker_rm_none(){
+#    EXITED_CONTAINER=$(docker ps -a | grep "Exited" | awk '{print $1 }')
+#    if [ ! -z "$EXITED_CONTAINER" ];
+#    then
+#        docker stop  $EXITED_CONTAINER
+#        docker rm  $EXITED_CONTAINER 
+#    fi
+#
+#    NONE_IMAGES=$(docker images | grep "none" | awk '{print $3}')
+#    if [ ! -z "$NONE_IMAGES" ];
+#    then
+#        echo rm $NONE_IMAGES over
+#        docker rmi  -f $NONE_IMAGES
+#    fi
+#}
