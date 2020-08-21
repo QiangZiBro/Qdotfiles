056a15e5d622fdee3915fab0896b22be15dd9fc2
QiangZiBro <qiangzibro@gmail.com>
2020-08-21 12:16:22 +0800

    update from ci

--- .ci/update.sh
+++ .ci/update.sh
@@ -2,13 +2,13 @@ set -ex
 cd $(dirname $0)/..
 
 if [ "$1" = "push" ];then
-    git pull origin master
+    git pull origin HEAD
     bash ~/.Qdotfiles/scripts/backup.sh
     git add -A && git commit -m 'update from ci'
-    git push origin master
+    git push origin HEAD
 
 elif [ "$1" = "pull" ];then
-    git pull origin master
+    git pull origin HEAD
     if [ "$2" = "all" ];then
         ssh l1 "/bin/bash /home/qiangzibro/.Qdotfiles/.ci/update.sh pull" &# ssh执行远程脚本
         ssh l2 "/bin/bash /home/qiangzibro/.Qdotfiles/.ci/update.sh pull" &# ssh执行远程脚本
