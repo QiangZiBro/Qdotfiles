5a69ffe175781014d439c3a9cb82745fcde55ca5
QiangZiBro <qiangzibro@gmail.com>
2020-08-21 15:24:53 +0800

    fix bug

--- scripts/setup_ss_privoxy_port.sh
+++ scripts/setup_ss_privoxy_port.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # set -x
 cd $(dirname $0)/..
-SS_PORT=
 SS_PORT_IN_FILES="privoxy/config ss/ss.json"
-PRIVOXY_PORT=
 PRIVOXY_PORT_IN_FILES="docker-compose.yml scripts/cproxy ss/proxy.zsh privoxy/config"
 
-read -p "ss port[default 1080]:" SS_PORT
+read -p "ss port [default 1080]:" SS_PORT
+SS_PORT=${SS_PORT:-1080}
 read -p "privoxy port [default 8118]:" PRIVOXY_PORT
+PRIVOXY_PORT=${PRIVOXY_PORT:-8118}
 
 echo SS_PORT:"$SS_PORT", PRIVOXY_PORT:"$PRIVOXY_PORT"
-echo make change in $SS_PORT_IN_FILES $PRIVOXY_PORT_IN_FILES
-
 
 while true; do
     read -p "Are you sure? Y/n:" yn
     case $yn in
         [Yy]* ) 
-            sed "s|1080|$SS_PORT|g" $SS_PORT_IN_FILES
-            sed "s|8118|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
+            if test "$(uname)" = "Darwin"
+            then
+            
+                sed -i '' "s|1080|$SS_PORT|g" $SS_PORT_IN_FILES
+                sed -i '' "s|8118|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
+            else
+                sed -i "s|1080|$SS_PORT|g" $SS_PORT_IN_FILES
+                sed -i "s|8118|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
+            fi
             break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
