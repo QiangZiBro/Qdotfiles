ed894cc311f33257e7f707270621e3c681281f87
QiangZiBro <qiangzibro@gmail.com>
2020-09-07 15:46:56 +0800

    add ss.json upload script

---
+++ scripts/update_ss_configure.sh
@@ -0,0 +1,17 @@
+update_ss(){
+    set -ex
+    cd $(dirname $0)/../ss
+    if [ ! -z $1 ]
+    then
+        cat export.json| jq '.configs' | jq '[.[] | select(.server=="$1")][0]' > ss.json
+    fi
+}
+
+upload_ss_configure(){
+    set +e
+    scp -o ConnectTimeout=5 ss.json l1:~/.Qdotfiles/ss/
+    scp -o ConnectTimeout=5 ss.json l2:~/.Qdotfiles/ss/
+}
+
+update_ss "$@"
+upload_ss_configure
