f9d9a614a0e49c76e2e56611e9f1e4e9caaafb73
QiangZiBro <qiangzibro@gmail.com>
2020-09-07 16:42:24 +0800

    update from ci

--- .ci/update.sh
+++ .ci/update.sh
@@ -1,20 +1,23 @@
 set -ex
 cd $(dirname $0)/..
-
-if [ "$1" = "push" ];then
-    git pull origin HEAD
-    bash ~/.Qdotfiles/scripts/backup.sh
-    if [ -n "$2" ];then
-        git add -A && git commit -m "$2"
-    else
-        git add -A && git commit -m 'update from ci'
+main(){
+    if [ "$1" = "push" ];then
+        git pull origin HEAD
+        bash ~/.Qdotfiles/scripts/backup.sh
+        if [ -n "$2" ];then
+            git add -A && git commit -m "$2"
+        else
+            git add -A && git commit -m 'update from ci'
+        fi
+        git push origin HEAD
+    
+    elif [ "$1" = "pull" ];then
+        git pull origin HEAD
+        if [ "$2" = "all" ];then
+            ssh l1 "/bin/bash /home/qiangzibro/.Qdotfiles/.ci/update.sh pull" &# ssh执行远程脚本
+            ssh l2 "/bin/bash /home/qiangzibro/.Qdotfiles/.ci/update.sh pull" &# ssh执行远程脚本
+        fi
     fi
-    git push origin HEAD
+}
 
-elif [ "$1" = "pull" ];then
-    git pull origin HEAD
-    if [ "$2" = "all" ];then
-        ssh l1 "/bin/bash /home/qiangzibro/.Qdotfiles/.ci/update.sh pull" &# ssh执行远程脚本
-        ssh l2 "/bin/bash /home/qiangzibro/.Qdotfiles/.ci/update.sh pull" &# ssh执行远程脚本
-    fi
-fi
+main "$@"
