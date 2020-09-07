922a9be9d4b14387a98d0294ab7a1d4b22bf2b8f
QiangZiBro <qiangzibro@gmail.com>
2020-09-07 16:19:17 +0800

    update from ci

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd "$(dirname $0)"/..
+sudo_cp(){
+    if [ -f "$1" ];
+    then
+        sudo cp "$1" "$2"
+    fi
+}
 
 cp ~/.config/nvim/init.vim neovim
 cp ~/.zshrc zsh
 cp ~/.tmux.conf tmux
-
-if [ -f /etc/privoxy/config ];
-then
-    sudo cp /etc/privoxy/config privoxy
-fi
+sudo_cp /etc/privoxy/config privoxy
