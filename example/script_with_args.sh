d0e26566f100205eb6111e80ee942338bb2e0740
QiangZiBro <qiangzibro@gmail.com>
2020-09-09 20:44:15 +0800

    update from ci

--- example/script_with_args.sh
+++ example/script_with_args.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+# ref1:https://gist.github.com/hfossli/4368aa5a577742c3c9f9266ed214aa58
+# ref2:https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -d|--deploy) deploy="$2"; shift ;;
-        -u|--uglify) uglify=1 ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
+CLEAR='\033[0m'
+RED='\033[0;31m'
 
-echo "Should deploy? $deploy"
-echo "Should uglify? $uglify"
+function usage() {
+  if [ -n "$1" ]; then
+    echo -e "${RED}☺️ $1${CLEAR}\n";
+  fi
+  echo "Usage: $0 [-n number-of-people] [-s section-id] [-c cache-file]"
+  echo "  -n, --number-of-people   The number of people"
+  echo "  -s, --section-id         A sections unique id"
+  echo "  -q, --quiet              Only print result"
+  echo ""
+  echo "Example: $0 --number-of-people 2 --section-id 1"
+  exit 1
+}
+
+# parse params
+while [[ "$#" > 0 ]]; do case $1 in
+  -n|--number-of-people) NUMBER_OF_PEOPLE="$2"; shift;shift;;
+  -s|--section-id) SECTION_ID="$2";shift;shift;;
+  -v|--verbose) VERBOSE=1;shift;;
+  *) usage "Unknown parameter passed: $1"; shift; shift;;
+esac; done
+
+# verify paams
+if [ -z "$NUMBER_OF_PEOPLE" ]; then usage "Number of people is not set"; fi;
+if [ -z "$SECTION_ID" ]; then usage "Section id is not set."; fi;
+
