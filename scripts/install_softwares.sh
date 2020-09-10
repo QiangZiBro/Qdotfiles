994fdeeb708aec591615a582150cb104d25324e4
QiangZiBro <qiangzibro@gmail.com>
2020-09-10 16:41:52 +0800

    update from ci

--- scripts/install_softwares.sh
+++ scripts/install_softwares.sh
@@ -6,6 +6,6 @@ cd "$(dirname $0)"/..
 for i in  common zsh neovim zlua #ccat
 do
     echo installing $i
-    bash $i/install.sh 2>&1 > /dev/null 
+    bash $i/install.sh #2>&1 > /dev/null 
 done
 wait
