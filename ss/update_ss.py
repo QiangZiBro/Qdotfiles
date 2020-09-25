c113856334866db34f0414666ea6c512a9d37420
QiangZiBro <qiangzibro@gmail.com>
2020-09-25 13:36:10 +0800

    update from ci

--- ss/update_ss.py
+++ ss/update_ss.py
@@ -8,12 +8,20 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-i","--ip", type=str, default="")
+parser.add_argument("-p","--port", type=str, default="")
 parser.add_argument("-f","--json_file", type=str, default="export.json")
 parser.add_argument("-r","--remote", default=False, action="store_true")
 args = parser.parse_args()
 
 json_file = args.json_file
 ip = args.ip
+port = args.port
+
+def write(c):
+    with open('ss.json', 'w') as outfile:
+        json.dump(c, outfile, indent=4)
+    print("I get it:")
+    print(c)
 
 with open(json_file) as json_data:
     data = json.load(json_data)
@@ -21,12 +29,15 @@ with open(json_file) as json_data:
 OK = False
 for c in data["configs"]:
     if c["server"] == ip:
-        OK = True
-        with open('ss.json', 'w') as outfile:
-            json.dump(c, outfile, indent=4)
-        print("I get it:")
-        print(c)
-        break
+        if port != "":
+            if str(c["server_port"]) == port:
+                OK = True
+                write(c)
+                break
+        else:
+            OK = True
+            write(c)
+            break
 if not OK:
     print(f"I did't find any configuration in your {json_file} file")
 
--- ss/update_ss.sh
+++ ss/update_ss.sh
@@ -1,4 +1,4 @@
 [ -z $2 ] && file="export.json" || file=$2
 if [ ! -z "$1" ];then
-	python ./update_ss.py -i $1 -f $file  -r
+	python ./update_ss.py -i $1 -p "$2" -f $file  -r
 fi
