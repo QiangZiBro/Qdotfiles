2abe2aaa7ecf39dccaaea2201467791ec7054f64
QiangZiBro <qiangzibro@gmail.com>
2020-09-26 20:08:36 +0800

    update from ci

---
+++ scripts/init_new_machine/install_desktop.sh
@@ -0,0 +1,6 @@
+# go to http://www.c-nergy.be/products.html
+set -e
+wget -c http://c-nergy.be/downloads/xRDP/xrdp-installer-1.2.zip
+unzip xrdp-installer-1.2.zip
+bash xrdp-installer-1.2.sh
+rm xrdp-installer-1.2.sh
