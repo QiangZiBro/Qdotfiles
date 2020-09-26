de07c28d3703e6df7e444be80e5afebb9fa938fe
QiangZiBro <qiangzibro@gmail.com>
2020-09-26 17:37:09 +0800

    update from ci

---
+++ scripts/cuda_install_preset.sh
@@ -0,0 +1,16 @@
+disable_nouveau(){
+	cat << EOF >>/etc/modprobe.d/blacklist-nouveau.conf
+	blacklist nouveau
+	options nouveau modeset=0
+	EOF
+	
+	update-initramfs -u
+	reboot
+}
+
+if [ "$1" = "-r" ];then
+	disable_nouveau
+fi
+
+sudo apt-get install -y g++ freeglut3-dev build-essential \
+	libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
