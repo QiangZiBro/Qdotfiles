2cd073f7eeca724ecc038f8091c4f48b9557fad7
QiangZiBro <qiangzibro@gmail.com>
2020-09-29 11:37:18 +0800

    update from ci

--- scripts/init_new_machine/cuda_install_preset.sh
+++ scripts/init_new_machine/cuda_install_preset.sh
@@ -1,13 +1,11 @@
 #---------------------------------------------------------------------------------
 #  安装cuda驱动之前必须要进行的配置
 # 
-#	  第一步： 
-#	  sudo bash ./cuda_install_preset.sh -r
+#	  第一步：只装软件去掉-r参数
+#		sudo bash ./cuda_install_preset.sh -r
 #	  第二步：
-#	  sudo bash ./cuda_install_preset.sh
-#	  第三步：
 #	  在https://developer.nvidia.com/cuda-downloads找到你的驱动runfile，下载，然后运行
-#	  ./cuda_*linux.run 
+#		./cuda_*linux.run 
 #---------------------------------------------------------------------------------
 disable_nouveau(){
 cat << EOF >>/etc/modprobe.d/blacklist-nouveau.conf
@@ -19,7 +17,22 @@ EOF
 	reboot
 }
 
+install_basic_softwares(){
+	sudo apt-get install -y g++ freeglut3-dev build-essential \
+		libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+}
+
+
+
+if [ ! "$(expr substr $(uname -s) 1 5)" = "Linux" ]
+then
+	echo Your system is not linux, abort!
+	exit 1
+fi
+
+
 if [ "$1" = "-r" ];then
+	install_basic_softwares
 	disable_nouveau
 
 elif [ "$1" = "-h" ];
@@ -33,14 +46,8 @@ cat << EOF
 		-h 打印这个帮助文档并退出
 		默认下载必备软件
 EOF
-
 exit 0
-fi
 
-if [ ! "$(expr substr $(uname -s) 1 5)" = "Linux" ]
-then
-	echo Your system is not linux, abort!
-	exit 1
+else
+	install_basic_softwares
 fi
-sudo apt-get install -y g++ freeglut3-dev build-essential \
-	libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
