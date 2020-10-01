84d40e9c45f90d475b63a8f5721456c23697ca50
QiangZiBro <qiangzibro@gmail.com>
2020-10-01 14:29:36 +0800

    修改tmux安装脚本

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,12 +4,12 @@ set -e
 if ! command -v  tmux &> /dev/null
 then
     # installation process...
-    if test "$(uname)" = "darwin"
+    if test "$(uname)" = "Darwin"
     then
          # mac installation branch
          brew install tmux
 
-    elif test "$(expr substr $(uname -s) 1 5)" = "linux"
+    elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
     then
          # linux installation branch
          sudo apt install tmux -y
@@ -17,9 +17,13 @@ then
 fi
 
 # tmux plug manager
-dir="~/.tmux/plugins/tpm"
-if [ ! -d $dir ];then
-	git clone https://github.com/tmux-plugins/tpm $dir
+if [ ! -d ~/.tmux/plugins/ ];then
+	mkdir -p ~/.tmux/plugins/
+fi
+
+if [ ! -d ~/.tmux/plugins/tqm ];then
+	cd ~/.tmux/plugins/
+	git clone https://github.com/tmux-plugins/tpm
 fi
 tmux source ~/.tmux.conf
 
