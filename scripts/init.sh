9566a0a1b7ea395a8685a872620acb4fa9b199cd
QiangZiBro <qiangzibro@gmail.com>
2020-10-02 15:04:08 +0800

    完善init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# 交互式模式的初始化脚本
+# 防止被加载两次
+if [ -z "$_INIT_SH_LOADED" ]; then
+    _INIT_SH_LOADED=1
+else
+    return
+fi
+
+# 如果是非交互式则退出，比如 bash test.sh 这种调用 bash 运行脚本时就不是交互式
+# 只有直接敲 bash 进入的等待用户输入命令的那种模式才成为交互式，才往下初始化
+case "$-" in
+    *i*) ;;
+    *) return
+esac
+
 source_if_exists(){
     file="$1"
 
@@ -43,3 +58,20 @@ source ~/.Qdotfiles/nvm/nvm.zsh
 if [ -e ~/.zsh_profile ];then
 	. ~/.zsh_profile
 fi
+
+# 整理 PATH，删除重复路径
+if [ -n "$PATH" ]; then
+    old_PATH=$PATH:; PATH=
+    while [ -n "$old_PATH" ]; do
+        x=${old_PATH%%:*}      
+        case $PATH: in
+           *:"$x":*) ;;         
+           *) PATH=$PATH:$x;;  
+        esac
+        old_PATH=${old_PATH#*:}
+    done
+    PATH=${PATH#:}
+    unset old_PATH x
+fi
+
+export PATH
