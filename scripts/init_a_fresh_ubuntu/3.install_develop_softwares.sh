478d592ff665663d1b8d6c82902dc9968d399329
QiangZiBro <qiangzibro@gmail.com>
2020-12-13 13:59:38 +0800

    update from ci

--- scripts/init_a_fresh_ubuntu/3.install_develop_softwares.sh
+++ scripts/init_a_fresh_ubuntu/3.install_develop_softwares.sh
@@ -1,2 +1,5 @@
-sudo snap install pycharm-professional pycharm-community code --classic
+sudo snap install pycharm-professional --classic
+sudo snap install pycharm-community --classic
+sudo snap install code --classic
+
 sudo snap install firefox chromium typora meshlab
