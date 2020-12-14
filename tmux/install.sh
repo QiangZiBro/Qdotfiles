6e972309b6894ae89fc680bb217d90b40f435223
QiangZiBro <qiangzibro@gmail.com>
2020-12-14 18:13:43 +0800

    tmux install

--- tmux/install.sh
+++ tmux/install.sh
@@ -10,22 +10,18 @@ then
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
 	if [ ! -f /usr/local/bin/tmux ];then
-		if [ `tmux -V` != "tmux ${version}" ];then
 			#https://gist.github.com/joelrich/2f6fa444649adaae8b8499e7b3a5769e
 			sudo apt update
 			sudo apt install -y build-essential autoconf automake pkg-config libevent-dev libncurses5-dev bison byacc
 			if [ -e /tmp/tmux ];then
 				rm -rf /tmp/tmux
 			fi
-			git clone https://github.com/tmux/tmux.git --depth 1 /tmp/tmux
+			git clone https://github.com/tmux/tmux.git /tmp/tmux
 			cd /tmp/tmux
 			git checkout "$version"
 			sh autogen.sh
 			./configure && make
 			sudo make install
-		else
-			echo "Tmux ${version} already exists, do nothing"
-		fi
 	fi
 fi
 
