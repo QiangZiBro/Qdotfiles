5b4d840e49e3b7d5913b5356a4ac272d8e95e692
QiangZiBro <qiangzibro@gmail.com>
2020-12-15 20:22:53 +0800

    update from ci

--- scripts/init_a_fresh_ubuntu/2.install_common_softwares.sh
+++ scripts/init_a_fresh_ubuntu/2.install_common_softwares.sh
@@ -6,7 +6,7 @@ then
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
 	softwares="git vim zsh silversearcher-ag\
-		       ranger curl tree make htop nodejs\
+		       ranger curl tree make htop nodejs npm\
 			   snapd docker docker.io docker-compose"
 	echo "Hi handsome user! I will install some very common softwares\
 		you may need such as $softwares"
