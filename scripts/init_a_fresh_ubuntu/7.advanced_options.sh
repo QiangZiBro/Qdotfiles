5b3f41297739e91cb3cd2ffc589385934815b7a8
QiangZiBro <qiangzibro@gmail.com>
2021-07-21 22:12:12 +0800

    nvidia docker

--- scripts/init_a_fresh_ubuntu/7.advanced_options.sh
+++ scripts/init_a_fresh_ubuntu/7.advanced_options.sh
@@ -1,1 +1,7 @@
 sudo npm install -g diff-so-fancy
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \\n   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \\n   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/experimental/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+
