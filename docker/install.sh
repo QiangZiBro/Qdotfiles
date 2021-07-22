bfcb53d531b74c63adf5a46570a9672297d3f554
QiangZiBro <qiangzibro@gmail.com>
2021-07-22 19:49:21 +0800

    docker install

--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+_docker_proxy(){
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo cat << EOF > /etc/systemd/system/docker.service.d/proxy.conf
+[Service]
+Environment="http_proxy=$http_proxy"
+Environment="https_proxy=$https_proxy"
+Environment="no_proxy="localhost,127.0.0.1,::1"
+EOF
+}
+
+_docker_gpu(){
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+}
+
 if test "$(uname)" = "Darwin"
 then
      # mac installation branch
@@ -9,5 +28,10 @@ then
      # linux installation branch
     echo linux install
 	sudo apt install -y docker docker.io docker-compose 
+	_docker_gpu
+	_docker_proxy
+
+	sudo systemctl daemon-reload
+	sudo systemctl restart docker
 
 fi
