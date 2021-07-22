138a9e5f8062166efa01a16f35042046326d1e15
QiangZiBro <qiangzibro@gmail.com>
2021-07-22 16:35:22 +0800

    ss

--- ss/proxy.zsh
+++ ss/proxy.zsh
@@ -18,6 +18,10 @@ function proxy(){
         export http_proxy=""
         export https_proxy=""
         git config --global https.proxy ''
+	elif [ "$1" = "cmd" ];then
+        echo export http_proxy="http://127.0.0.1:$PROXY_PORT"
+        echo export https_proxy="https://127.0.0.1:$PROXY_PORT"
+        echo git config --global https.proxy https://127.0.0.1:$PROXY_PORT
     else
         echo "Wrong parameter!Usage: proxy [start|stop]"
     fi
