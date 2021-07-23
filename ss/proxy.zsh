3498d702fed8235a74bb5483bcb9bd94e6c91ba0
QiangZiBro <qiangzibro@gmail.com>
2021-07-23 16:55:12 +0800

    ss

--- ss/proxy.zsh
+++ ss/proxy.zsh
@@ -2,36 +2,34 @@ function cg(){
 	curl -I --silent www.google.com | head -n 1 | awk -F' ' '{print $2}'
 }
 function proxy(){
-    if test "$(uname)" = "Darwin";then
-        # mac branch
-        PROXY_PORT=1087
-    elif test "$(expr substr $(uname -s) 1 5)" = "Linux";then
-        # linux branch
-        PROXY_PORT=8999
-    fi
+	if test "$(uname)" = "Darwin";then
+		PROXY_PORT=1087
+	elif test "$(expr substr $(uname -s) 1 5)" = "Linux";then
+		PROXY_PORT=8999
+	fi
 
-    if [ "$1" = "start" ]; then
-        export http_proxy="127.0.0.1:$PROXY_PORT"
-        export https_proxy="127.0.0.1:$PROXY_PORT"
+	if [ "$1" = "start" ]; then
+		export http_proxy="127.0.0.1:$PROXY_PORT"
+		export https_proxy="127.0.0.1:$PROXY_PORT"
 		# I found that setting http/https proxy directly works for git
-        #git config --global https.proxy https://127.0.0.1:$PROXY_PORT
-    elif [ "$1" = "stop" ]; then
-        export http_proxy=""
-        export https_proxy=""
-        git config --global https.proxy ''
+		#git config --global https.proxy https://127.0.0.1:$PROXY_PORT
+	elif [ "$1" = "stop" ]; then
+		export http_proxy=""
+		export https_proxy=""
+		git config --global https.proxy ''
 	elif [ "$1" = "cmd" ];then
-        echo export http_proxy="http://127.0.0.1:$PROXY_PORT"
-        echo export https_proxy="https://127.0.0.1:$PROXY_PORT"
-        #echo git config --global https.proxy https://127.0.0.1:$PROXY_PORT
+		echo export http_proxy="http://127.0.0.1:$PROXY_PORT"
+		echo export https_proxy="https://127.0.0.1:$PROXY_PORT"
+		#echo git config --global https.proxy https://127.0.0.1:$PROXY_PORT
 	elif [ "$1" = "which" ];then
 		cat ~/.Qdotfiles/ss/ss.json
 	elif [ "$1" = "test" ];then
 		cg
 	elif [ "$1" = "set" ];then
 		change_ss "${@:2}"
-    else
-        echo "Wrong parameter!Usage: proxy [start|stop|cmd|which|test]"
-    fi
+	else
+		echo "Wrong parameter!Usage: proxy [start|stop|cmd|which|test]"
+	fi
 }
 
 function change_ss(){
