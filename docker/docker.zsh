c32935db6c6966fb6915a99458a121082639ff3c
QiangZiBro <qiangzibro@gmail.com>
2021-07-28 14:08:19 +0800

    docker rm none

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -5,18 +5,8 @@ alias dce="docker-compose exec"
 alias dcb="docker-compose build"
 alias ddown="docker-compose down"
 
-#docker_rm_none(){
-#    EXITED_CONTAINER=$(docker ps -a | grep "Exited" | awk '{print $1 }')
-#    if [ ! -z "$EXITED_CONTAINER" ];
-#    then
-#        docker stop  $EXITED_CONTAINER
-#        docker rm  $EXITED_CONTAINER 
-#    fi
-#
-#    NONE_IMAGES=$(docker images | grep "none" | awk '{print $3}')
-#    if [ ! -z "$NONE_IMAGES" ];
-#    then
-#        echo rm $NONE_IMAGES over
-#        docker rmi  -f $NONE_IMAGES
-#    fi
-#}
+docker_rm_none(){
+	docker stop $(docker ps -a | grep "Exited" | awk '{print $1 }') #停止容器
+	docker rm $(docker ps -a | grep "Exited" | awk '{print $1 }') #删除容器
+	docker rmi $(docker images | grep "none" | awk '{print $3}') #删除镜像
+}
