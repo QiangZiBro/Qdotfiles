49edd040f44f0709e451378e0dd331f128f8644d
QiangZiBro <qiangzibro@gmail.com>
2021-07-28 15:25:32 +0800

    docker add mac image server

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -10,3 +10,6 @@ docker_rm_none(){
 	docker rm $(docker ps -a | grep "Exited" | awk '{print $1 }') #删除容器
 	docker rmi $(docker images | grep "none" | awk '{print $3}') #删除镜像
 }
+docker_show(){
+	socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\"
+}
