3d556db542187ff3cadd41b6199c6b4484b44a35
QiangZiBro <qiangzibro@gmail.com>
2021-08-18 14:41:17 +0800

    update doc

--- scripts/setup_ss_privoxy_port.sh
+++ scripts/setup_ss_privoxy_port.sh
@@ -2,12 +2,12 @@
 # set -x
 cd $(dirname $0)/..
 SS_PORT_IN_FILES="privoxy/config ss/ss.json scripts/setup_ss_privoxy_port.sh"
-PRIVOXY_PORT_IN_FILES="docker-compose.yml scripts/cproxy ss/proxy.zsh privoxy/config scripts/setup_ss_privoxy_port.sh"
+PRIVOXY_PORT_IN_FILES="docker-compose.yml scripts/cproxy ss/proxy.zsh privoxy/config scripts/setup_ss_privoxy_port.sh .env"
 
 read -p "ss port [default 1080]:" SS_PORT
 SS_PORT=${SS_PORT:-1080}
-read -p "privoxy port [default 8999]:" PRIVOXY_PORT
-PRIVOXY_PORT=${PRIVOXY_PORT:-8999}
+read -p "privoxy port [default 9090]:" PRIVOXY_PORT
+PRIVOXY_PORT=${PRIVOXY_PORT:-9090}
 
 echo SS_PORT:"$SS_PORT", PRIVOXY_PORT:"$PRIVOXY_PORT"
 
@@ -19,10 +19,10 @@ while true; do
             then
             
                 sed -i '' "s|1080|$SS_PORT|g" $SS_PORT_IN_FILES
-                sed -i '' "s|8999|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
+                sed -i '' "s|9090|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
             else
                 sed -i "s|1080|$SS_PORT|g" $SS_PORT_IN_FILES
-                sed -i "s|8999|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
+                sed -i "s|9090|$PRIVOXY_PORT|g" $PRIVOXY_PORT_IN_FILES
             fi
             break;;
         [Nn]* ) exit;;
