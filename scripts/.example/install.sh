a2b187bf68b1ad97aaba26b57613f727ffc78b44
QiangZiBro <qiangzibro@gmail.com>
2021-08-19 11:47:24 +0800

    example directory

--- scripts/.example/install.sh
+++ scripts/.example/install.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-if test "$(uname)" = "Darwin"
-then
-     # mac installation branch
-    echo mac install
-
-elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-then
-     # linux installation branch
-    echo linux install
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo linux install
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo darwin install
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	echo cygwin install
+elif [[ "$OSTYPE" == "msys" ]]; then
+	echo msys install
+elif [[ "$OSTYPE" == "win32" ]]; then
+	echo win32 install
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	echo freebsd install
+else
+	echo not supported for current system
 fi
