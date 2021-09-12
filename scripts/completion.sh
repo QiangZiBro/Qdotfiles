e60eb10cc2155f424ee7be76669254796b64c8c0
QiangZiBro <qiangzibro@gmail.com>
2021-09-12 15:44:35 +0800

    feature: command completion

---
+++ scripts/completion.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env bash
+complete -W "pull push bootstrap backup" qdot
+complete -W "on off up down set status" proxy
+complete -W "ip hs heic" qtool
--- scripts/init.sh
+++ scripts/init.sh
@@ -59,6 +59,9 @@ source ~/.Qdotfiles/tmux/tmux.zsh
 
 # fuck
 command -V thefuck 2>&1 >/dev/null && eval $(thefuck --alias)
+
+# 命令补全
+source ~/.Qdotfiles/scripts/completion.sh
 # 整理 PATH，删除重复路径
 if [ -n "$PATH" ]; then
   old_PATH=$PATH:
