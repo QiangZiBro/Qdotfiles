d2137b0f6c2b9cc5faf55136e618db1609c7e04a
QiangZiBro <qiangzibro@gmail.com>
2021-09-15 16:22:29 +0800

    remove unnecesary commands

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,15 +8,15 @@ set -ex
 # 4. install theme file
 
 if test "$(uname)" = "Darwin"; then
-  # mac installation branch
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-  echo installing zsh
-  # linux installation branch
   wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -qO - | zsh
   cp ~/.Qdotfiles/zsh/.zshrc ~/.zshrc
   sudo chsh $USER -s $(which zsh)
 fi
 
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh && source ~/.Qdotfiles/zsh/theme.zsh
+if ! test -d; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+  source ~/.Qdotfiles/zsh/theme.zsh
+fi
