6112fda2f425be7aaa6896b562ccfd368101bcb8
QiangZiBro <qiangzibro@gmail.com>
2021-09-15 14:25:18 +0800

    neovim install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-if test "$(uname)" = "Darwin"; then
-  brew install neovim
-elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-  # Use neovim for all users
-  if [ ! -f /usr/local/bin/nvim ]; then
-    cd /usr/local/softwares/
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-    chmod 777 ./nvim.appimage
-    ./nvim.appimage --appimage-extract
-    mv squashfs-root nvim
-    sudo ln -s $(realpath nvim/AppRun) /usr/local/bin/nvim
+
+# 安装neovim
+if ! command -v nvim 2>&1 >/dev/null; then
+  if test "$(uname)" = "Darwin"; then
+    brew install neovim
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
+    sudo apt install neovim
   fi
+else
+  echo nvim has been installed in your system!
 fi
 
-## 安装插件管理器
+## 安装vim插件管理器
 if ! test -e "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
 ## 安装插件
-#nvim +PlugInstall +qall
+if test -e "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim; then
+  nvim +PlugInstall +qall
+else
+  echo vim plug manager is not existed in the system, abort plugin install!
+  exit 1
+fi
+exit 0
--- scripts/.example/install.sh
+++ scripts/.example/install.sh
@@ -1,15 +1,19 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo linux install
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo darwin install
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-  echo cygwin install
-elif [[ "$OSTYPE" == "msys" ]]; then
-  echo msys install
-elif [[ "$OSTYPE" == "win32" ]]; then
-  echo win32 install
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-  echo freebsd install
+if command -v $1 2>&1 >/dev/null; then
+  echo $1 has been installed
 else
-  echo not supported for current system
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo linux install
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo darwin install
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo cygwin install
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    echo msys install
+  elif [[ "$OSTYPE" == "win32" ]]; then
+    echo win32 install
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo freebsd install
+  else
+    echo not supported for current system
+  fi
 fi
