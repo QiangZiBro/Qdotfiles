dbf1d3b74663f7501b2ab2f4cb635ead36678577
QiangZiBro <qiangzibro@gmail.com>
2021-09-23 14:06:16 +0800

    fix tmux install

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if test ! $(which docker); then
+if test ! $(which tmux); then
   if test "$(uname)" = "Darwin"; then
     brew install tmux
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
