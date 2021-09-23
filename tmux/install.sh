06bd24b109678b3c5a6458fa925910f67b348473
QiangZiBro <qiangzibro@gmail.com>
2021-09-23 14:04:29 +0800

    tmux install

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-version="3.0a"
-
-set -e
-if test "$(uname)" = "Darwin"; then
-  # mac installation branch
-  brew install tmux
-
-elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-  if [ ! -f /usr/local/bin/tmux ]; then
-    #https://gist.github.com/joelrich/2f6fa444649adaae8b8499e7b3a5769e
-    sudo apt update
-    sudo apt install -y build-essential autoconf automake pkg-config libevent-dev libncurses5-dev bison byacc
-    if [ -e /tmp/tmux ]; then
-      rm -rf /tmp/tmux
-    fi
-    git clone https://github.com/tmux/tmux.git /tmp/tmux
-    cd /tmp/tmux
-    git checkout "$version"
-    sh autogen.sh
-    ./configure && make
-    sudo make install
+if test ! $(which docker); then
+  if test "$(uname)" = "Darwin"; then
+    brew install tmux
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
+    sudo apt install tmux
   fi
 fi
 
