b4e6cce7b0f6a023d50bc9656f32f776428395e3
QiangZiBro <qiangzibro@gmail.com>
2021-11-26 14:27:43 +0800

    update docker install script

--- docker/install.sh
+++ docker/install.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 _setting_docker_runtime_proxy() {
+  if [ -d ~/.Qdotfiles ]; then
+    if [ -f ~/.Qdotfiles/.env ]; then
+      source ~/.Qdotfiles/.env
+    fi
+  fi
   sudo mkdir -p /etc/systemd/system/docker.service.d
   sudo cat <<EOF >/etc/systemd/system/docker.service.d/proxy.conf
 [Service]
-Environment="http_proxy=$http_proxy"
-Environment="https_proxy=$https_proxy"
+Environment="http_proxy=$HTTP_PROXY_PORT"
+Environment="https_proxy=$HTTP_PROXY_PORT"
 Environment="no_proxy="localhost,127.0.0.1,::1"
 EOF
 }
 
 _install_nvidia_docker() {
-if test ! $(which nvidia-docker); then
-  distribution=$(
-    . /etc/os-release
-    echo $ID$VERSION_ID
-  ) &&
-    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - &&
-    curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-  sudo apt-get update
-  sudo apt-get install -y nvidia-docker2
-fi
+  if test ! $(which nvidia-docker); then
+    distribution=$(
+      . /etc/os-release
+      echo $ID$VERSION_ID
+    ) &&
+      curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - &&
+      curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    sudo apt-get update
+    sudo apt-get install -y nvidia-docker2
+  fi
 }
 
 if test ! $(which docker); then
-  curl -sSL https://get.docker.com/ | sh 
+  curl -sSL https://get.docker.com/ | sh
   pip install docker-compose
 fi
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  #_setting_docker_runtime_proxy
   # 解决Linux下docker要sudo的问题
   GROUPNAME=docker
   getent group $GROUPNAME 2>&1 >/dev/null || groupadd $GROUPNAME
@@ -35,6 +39,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # 安装GPU支持的docker
   _install_nvidia_docker
 
+  #为Docker设置Http代理
+  #_setting_docker_runtime_proxy
   sudo systemctl daemon-reload
   sudo systemctl restart docker
 fi
