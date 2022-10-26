2925c4023c9f84ae31f955d3d88be61d7d4d6185
QiangZiBro <qiangzibro@gmail.com>
2022-10-26 11:14:45 +0800

    add tmk

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,10 +1,3 @@
-tns() {
-  tmux new -s "$@"
-}
-
-tat() {
-  tmux attach -t "$@"
-}
 # tm - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
 # `tm` will allow you to select your tmux session via fzf.
 # `tm irc` will attach to the irc session (if it exists), else it will create it.
@@ -14,4 +7,17 @@ tm() {
  tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
   fi
   session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+}
+
+tmk () {
+    local sessions
+    sessions="$(tmux ls|fzf --exit-0 --multi)"  || return $?
+    local i
+    for i in "${(f@)sessions}"
+    do
+        [[ $i =~ '([^:]*):.*' ]] && {
+            echo "Killing $match[1]"
+            tmux kill-session -t "$match[1]"
+        }
+    done
 }
